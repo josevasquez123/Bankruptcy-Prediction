0a2f249ca6f3e8555f059b9a76161fc23ab31c2f
Jose Vasquez <jose.vasquez.a@utec.edu.pe>
2023-01-01 21:15:16 -0500

    added API and docker

---
+++ api/schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+
+class ModelInput(BaseModel):
+    val_1:float
+    val_2:float
+    val_3:float
+    val_4:float
+    val_5:float
+    val_6:float
+    val_7:float
+    val_8:float
+    val_9:float
+    val_10:float
+    val_11:float
+    val_12:float
+    val_13:float
+    val_14:float
+    val_15:float
+    val_16:float
+    val_17:float
+    val_18:float
+    val_19:float
+    val_20:float
